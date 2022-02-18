75e15efa001cef6c8708b1c321914b6f95426e13
Dereje Ayele <derejeatomsa2@gmail.com>
2022-02-18 11:58:03 -0500

    first commit

---
+++ src/main/java/com/company/HelloHeroku/HelloHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.company.HelloHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHerokuApplication.class, args);
+	}
+
+}
